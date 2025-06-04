78a607da58b00c30cc99b2a15f5cff97c56cd1ef
Seiju Berg <seiju.berg@ruggedscience.com>
2025-06-04 13:55:03 -0400

    working conversion in extract.py

--- extract.py
+++ extract.py
@@ -2,8 +2,12 @@
 import shutil
 from os.path import relpath
 from typing import Any
-
+import pandoc
 from config import get_config
+
+def convert(src: str, dst: str):
+    tmp = pandoc.read(file=src)
+    pandoc.write(tmp, file=dst)
 
 
 def main(config: dict[str,Any]):
@@ -11,12 +15,16 @@ def main(config: dict[str,Any]):
         doc = config["documents"][i]
         archive = config["archives"][i]
         target = config["targets"][i]
+        markdown = target + ".md"
 
-        print(f"move {relpath(doc)} to {relpath(archive)}")
+        print(f"copy \t\t{relpath(doc)} \tto \t{relpath(archive)}")
         shutil.copy2(src=doc, dst=archive)
         
-        print(f"unpack {relpath(archive)} to {relpath(target)}")
+        print(f"unpack \t\t{relpath(archive)} \tto \t{relpath(target)}")
         shutil.unpack_archive(filename=archive, extract_dir=target)
+
+        print(f"converting \t{relpath(doc)} \tto \t{relpath(markdown)}")
+        convert(src=doc, dst=markdown)
 
 
 if __name__ == "__main__":
