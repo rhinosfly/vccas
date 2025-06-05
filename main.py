dc2ed68fbc3247c71b85e0c2cff1fc434dd02337
Seiju Berg <seiju.berg@ruggedscience.com>
2025-06-05 15:49:41 -0400

    add main prototype; calls verify

---
+++ main.py
@@ -0,0 +1,34 @@
+'''root of application'''
+
+import argparse
+import size
+import verify
+import convert
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--echo", help="echo given string", metavar="STRING")
+
+    subparsers = parser.add_subparsers(title="subcommands", required = True)
+    extracter = subparsers.add_parser("extract", help="extract source to target")
+    extracter.set_defaults(func=convert.make_xml)
+    archiver = subparsers.add_parser("archive", help="archive target to source")
+    archiver.set_defaults(func=convert.make_docx)
+    verifier  = subparsers.add_parser('verify', help="verify source matches archive")
+    verifier.set_defaults(func=verify.main)
+    measure = subparsers.add_parser("measure", help="measure size configured files")
+    measure.set_defaults(func=size.main)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    print(args)
+    args.func()
+
+
+if __name__ == "__main__":
+    main()    
